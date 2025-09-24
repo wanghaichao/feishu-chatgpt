0c597d916724deb63b7d1cb9f7e5d5472819306d
Evans Wang <evans.wang@BFF-PHF6W7GGP4.local>
2025-09-24 16:53:34 +0800

    search

--- code/utils/web.go
+++ code/utils/web.go
@@ -91,6 +91,7 @@ func WebSearch(query string, topK int) ([]SearchResult, error) {
 
 	// Extract <a class="result__a" href="...">Title</a>
 	anchorRe := regexp.MustCompile(`<a[^>]*class=\"result__a\"[^>]*href=\"([^\"]+)\"[^>]*>(.*?)</a>`)
+	fmt.Printf("[WebSearchresultsanchorRe] %s", anchorRe)
 	matches := anchorRe.FindAllStringSubmatch(htmlStr, -1)
 	var results []SearchResult
 	for _, m := range matches {
